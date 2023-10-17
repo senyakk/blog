83779e77a3dbffc6904f8785c299f22236488437
senyakk <arsenij@arsenij.com>
2023-10-17 21:23:34 +0200

    test

--- src/routes/Feed/SearchInput.tsx
+++ src/routes/Feed/SearchInput.tsx
@@ -8,7 +8,7 @@ const SearchInput: React.FC<Props> = ({ ...props }) => {
   return (
     <StyledWrapper>
       <div className="top">
-        <Emoji>🔍</Emoji> Search
+        🔍 Search
       </div>
       <input
         className="mid"
