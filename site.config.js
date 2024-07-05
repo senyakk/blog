4122e27d688bae3de3c5344407aeff1e19d2a20c
senyakk <arsenij@arsenij.com>
2024-07-05 12:59:51 +0300

    dfs

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     name: "Arseniy Golitsyn",
     image: "/profile_pic.JPG", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "narrowly known in wide circles",
-    bio: "Человек – это звучит гордо!",
+    bio: "Philosophy blog",
     // email: "arsenij@arsenij.com",
     //linkedin: "/arsenijs-golicins/",
     // github: "/senyakk",
